a4935d07d3623d557ac47c3a2ed464d8e47166c7
PPolar9600 <pollachaet@hotmail.com>
2023-10-30 06:58:55 +0700

    git

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -2,69 +2,68 @@ import Post from "../post/Post";
 import "./posts.scss"
 import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
-import { createClient } from '@tanstack/react-query';
 
 
 const Posts = () => {
 
-  const client = createClient({
-    defaultOptions: {
-       queries: {
-         // default query options
-       },
-       mutations: {
-         // default mutation options
-       },
-    },
-   });
+  // const client = createClient({
+  //   defaultOptions: {
+  //      queries: {
+  //        // default query options
+  //      },
+  //      mutations: {
+  //        // default mutation options
+  //      },
+  //   },
+  //  });
 
-   function Posts() {
-    const fetchPosts = async () => {
-       const response = await fetch('http://localhost:8080/auth/api/posts');
-       return response.json();
-    };
+  //  function Posts() {
+  //   const fetchPosts = async () => {
+  //      const response = await fetch('http://localhost:8080/auth/api/posts');
+  //      return response.json();
+  //   };
    
-    const { data, isLoading, error } = useQuery('posts', fetchPosts);
+  //   const { data, isLoading, error } = useQuery('posts', fetchPosts);
    
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.message}</div>;
+  //   if (isLoading) return <div>Loading...</div>;
+  //   if (error) return <div>Error: {error.message}</div>;
    
-    return (
-       <div>
-         {data.map((post) => (
-           <div key={post.id}>{post.title}</div>
-         ))}
-       </div>
-    );
-   }
+  //   return (
+  //      <div>
+  //        {data.map((post) => (
+  //          <div key={post.id}>{post.title}</div>
+  //        ))}
+  //      </div>
+  //   );
+  //  }
   
 
     //TEMPORARY
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     name: "John Doe",
-  //     userId: 1,
-  //     profilePic:
-  //       "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-  //     img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Jane Doe",
-  //     userId: 2,
-  //     profilePic:
-  //       "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
-  //   },
-  // ];
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+    },
+  ];
 
-  // return <div className="posts">
-  //   {posts.map(post=>(
-  //       <Post post={post} key={post.id}/>
-  //   ))} 
-  //   </div>;
+  return <div className="posts">
+    {posts.map(post=>(
+        <Post post={post} key={post.id}/>
+    ))} 
+    </div>;
 
 // const { isLoading, error, data } = useQuery(["posts"], () =>
 // makeRequest.get("/posts?userId="+userId).then((res) => {
